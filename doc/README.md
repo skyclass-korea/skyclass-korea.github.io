9260acb19096c51db249e1fac36f6fb655ca943d
imnotkind <imnotkind307@gmail.com>
2020-08-12 00:12:02 +0900

    vuepress

---
+++ doc/README.md
@@ -0,0 +1,8 @@
+# Today I Learned
+
+덕호가 오늘 새로 배운 것을 기록합니다. 
+
+![텍스트](https://i.imgur.com/sRzoLoa.jpg)
+
+> HOW TO
+- ./commit.sh "str"
---
+++ doc/docs/.vuepress/config.js
@@ -0,0 +1,92 @@
+module.exports = {
+    title: 'imnotkind wiki',
+    description: 'wiki',
+    base: '/doc/docs/.vuepress/dist',
+    themeConfig: {
+        logo: '/favicon.ico',
+        nav: [
+            { text: 'GitHub', link: 'https://github.com/imnotkind' },
+            { text: 'Homepage', link: 'https://skyclass.github.io' } 
+        ],
+        sidebar: getSidebarArr()
+    },
+    plugins: {
+        seo: {
+            siteTitle: (_, $site) => $site.title,
+            title: $page => $page.title,
+            description: $page => $page.frontmatter.description,
+            author: (_, $site) => $site.themeConfig.author,
+            tags: $page => $page.frontmatter.tags,
+            twitterCard: _ => 'summary',
+            type: $page =>
+                ['articles', 'posts', 'blog'].some(folder =>
+                    $page.regularPath.startsWith('/' + folder)
+                )
+                    ? 'article'
+                    : 'website',
+            url: (_, $site, path) => ($site.themeConfig.domain || '') + path,
+            image: ($page, $site) =>
+                $page.frontmatter.image &&
+                ($site.themeConfig.domain || '') + $page.frontmatter.image,
+            publishedAt: $page => $page.frontmatter.date && new Date($page.frontmatter.date),
+            modifiedAt: $page => $page.lastUpdated && new Date($page.lastUpdated)
+        }
+    }
+};
+function getSidebarArr() {
+    var fs = require('fs');
+    var docsPath = __dirname + '/../';
+    var sidebarArr = [];
+    var HomeFilelist = [];
+    var filelist = fs.readdirSync(docsPath);
+    filelist.forEach(function(file) {
+        if (file === '.vuepress') return;
+        var stat = fs.lstatSync(docsPath + '/' + file);
+        if (stat.isDirectory()) {
+            // directory
+            // title is file, children is readdirSync
+            var docsFolderPath = docsPath + '/' + file;
+            var list = fs.readdirSync(docsFolderPath);
+            sidebarArr.push(makeSidebarObject(file, list));
+        } else { 
+            // NOT directory
+            // title is '/' children is file
+            HomeFilelist.push(file);
+        }
+    });
+    sidebarArr.unshift(makeSidebarObject('', HomeFilelist));
+    console.log(sidebarArr)
+    return sidebarArr;
+}
+function makeSidebarObject(folder, mdfileList) {
+    var path = folder ? '/' + folder + '/' : '/';
+    mdfileList = aheadOfReadme(mdfileList);
+    var tmpMdfileList = [];
+    // remove .md, add Path
+    mdfileList.forEach(function(mdfile) {
+		if (mdfile.substr(-3) === '.md') {
+                mdfile = mdfile.slice(0, -3) === 'README' ? '' : mdfile.slice(0, -3);
+                tmpMdfileList.push([path + mdfile, mdfile]);
+        }
+    });
+    mdfileList = tmpMdfileList;
+    // remove folder prefix number
+    if (folder) {
+        var dotIdx = folder.indexOf('.');
+        var title = Number(folder.substr(0, dotIdx)) ? folder.substr(dotIdx + 1) : folder;
+    } else {
+        title = 'Home';
+    }
+    return {
+        title: title,
+        children: mdfileList
+    };
+}
+function aheadOfReadme(arr) {
+    // ['1.test.md','README.md'] => ['README.md','1.test.md']
+    var readmeIdx = arr.indexOf('README.md');
+    if (readmeIdx > 0) {
+        arr.unshift(arr.splice(readmeIdx, 1)[0]);
+    }
+    return arr;
+}
